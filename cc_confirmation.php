63e6274e5a7bb980101c3fd74f5ef0072a0ce68f
MLSMason6 <mschrader1@una.edu>
2025-02-15 16:39:00 -0600

    Update cc_confirmation.php

--- cc_confirmation.php
+++ cc_confirmation.php
@@ -5,11 +5,11 @@ function cc_confirmation($card) {
     $first_two_digits = $card[0] . $card[1];
     if ($first_two_digits >= "51" && $first_two_digits <= 55 && $card_length == 16) {
         echo "Thanks for your purchase";
-    } elseif ($first_digit == "4" && $card_length == 16) {
+    } elseif ($first_digit == "4" && ($card_length == 13 || $card_length == 16)) {
         echo "Thanks for your purchase";
     } elseif ($first_two_digits == "34" && $card_length == 15) {
         echo "Thanks for your purchase";
-        echo '<a href="index.php"><button>Go back to the Home Page</button</a>';
+        echo '<a href="index.php"><button>Go back to the Home Page</button></a>';
     } else {
         echo "Card number is invalid. Click the back arrow in the top-left corner. ";
     }
